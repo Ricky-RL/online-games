ec32a7f58b1b8ceb281aa61ae4f2f05fa16dff49
Ricky Lin <rickylin543@gmail.com>
2026-04-29 15:02:46 -0700

    feat(word-search): add theme pack definitions

---
+++ src/lib/word-search-themes.ts
@@ -0,0 +1,34 @@
+import type { ThemePack } from './word-search-types';
+
+export const THEME_PACKS: ThemePack[] = [
+  {
+    id: 'animals',
+    name: 'Animals',
+    words: ['TIGER', 'LION', 'ELEPHANT', 'GIRAFFE', 'PENGUIN', 'DOLPHIN', 'EAGLE', 'PYTHON', 'ZEBRA', 'MONKEY', 'RABBIT', 'FALCON', 'WHALE', 'PANDA', 'KOALA'],
+  },
+  {
+    id: 'food',
+    name: 'Food',
+    words: ['PIZZA', 'SUSHI', 'BURGER', 'TACO', 'PASTA', 'WAFFLE', 'MANGO', 'STEAK', 'SALMON', 'COOKIE', 'PRETZEL', 'RAMEN', 'CREPE', 'DONUT', 'BAGEL'],
+  },
+  {
+    id: 'countries',
+    name: 'Countries',
+    words: ['BRAZIL', 'JAPAN', 'FRANCE', 'EGYPT', 'CANADA', 'MEXICO', 'INDIA', 'SPAIN', 'ITALY', 'PERU', 'NORWAY', 'CHILE', 'CUBA', 'GHANA', 'NEPAL'],
+  },
+  {
+    id: 'sports',
+    name: 'Sports',
+    words: ['TENNIS', 'SOCCER', 'HOCKEY', 'RUGBY', 'BOXING', 'SKIING', 'DIVING', 'SURFING', 'ROWING', 'FENCING', 'GOLF', 'KARATE', 'POLO', 'SQUASH', 'ARCHERY'],
+  },
+  {
+    id: 'colors',
+    name: 'Colors',
+    words: ['CRIMSON', 'VIOLET', 'INDIGO', 'SCARLET', 'BRONZE', 'SILVER', 'GOLDEN', 'CORAL', 'IVORY', 'MAROON', 'TEAL', 'AMBER', 'JADE', 'RUBY', 'COBALT'],
+  },
+  {
+    id: 'movies',
+    name: 'Movies',
+    words: ['JAWS', 'FROZEN', 'ALIEN', 'ROCKY', 'SHREK', 'BAMBI', 'MULAN', 'BRAVE', 'COCO', 'DUNE', 'JOKER', 'TENET', 'MOANA', 'PSYCHO', 'GREASE'],
+  },
+];
